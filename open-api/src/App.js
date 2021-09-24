ef5ec9042435fb7bcce7b3ecfaecca0cc17cb977
hambb jung <lantan.rain@gmail.com>
2021-09-24 15:35:42 +0900

    added eslint-google, typescript modules

--- open-api/src/App.js
+++ open-api/src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import React from "react";
 
 class App extends React.Component {
-  componentDidMount () {
+  componentDidMount() {
     const script = document.createElement("script");
     script.src = "http://10.0.0.144:8080/js/test.js";
     script.async = true;
@@ -14,23 +14,22 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
+          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             Learn React
           </a>
         </header>
       </div>
-    )
+    );
   }
 }
-
 
 export default App;
