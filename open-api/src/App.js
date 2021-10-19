c78dae980e9b3790f762470e48f4154099ffc625
hambb jung <lantan.rain@gmail.com>
2021-10-19 14:38:36 +0900

    fixed browser router navigation

--- open-api/src/App.js
+++ open-api/src/App.js
@@ -2,18 +2,17 @@ import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import KetiNavigator from "./nav/KetiNavigator";
 import KetiOpenApi from "./openapi/KetiOpenApi";
+import Guide from "./openapi/Guide";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <KetiNavigator />
-        <BrowserRouter basename="/open-api">
-          <Route path="/" component={KetiOpenApi}></Route>
-        </BrowserRouter>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <KetiNavigator />
+      <Route exact path="/" component={Guide} />
+      <Route path="/guide" component={Guide} />
+      <Route path="/open-api" component={KetiOpenApi} />
+    </BrowserRouter>
+  );
+};
 
 export default App;
---
+++ open-api/src/openapi/Guide.tsx
@@ -0,0 +1,10 @@
+import React, { useContext } from "react";
+import { Container } from "react-bootstrap";
+
+class Guide extends React.Component {
+  render() {
+    return <Container>hello</Container>;
+  }
+}
+
+export default Guide;
